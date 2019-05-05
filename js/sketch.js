1105a6bd8d3bca47d0f4748c1815c7e547c66cd4
Ginner Zapata <ginnerzapata@gmail.com>
2019-05-05 20:52:49 -0300

    Fighting against the buttons

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,6 @@
 //DOM chaching
 const wrap_div = document.querySelector('.wrap');
 const container_div = document.querySelector('.container');
-const grid_div = document.querySelector('.grid');
-const gridActive_div = document.querySelector('.gridActive');
 const resetBtn = document.querySelector('.left');
 const gridBtn = document.querySelector('.right');
 
@@ -10,11 +8,6 @@ const gridBtn = document.querySelector('.right');
 //creating divs
 let defNumOfSq = 50;    
 const contSize = 300;    //the size of the container
-
-//adding color to the grid
-function paint() {
-    grid_div.setAttribute('class','grid-active');
-}
 
 //this fuction is the default grid
 
@@ -27,7 +20,7 @@ function paint() {
            div.setAttribute('style', `width: ${+sqSize}px; height: ${+sqSize}px`);
            container_div.appendChild(div);
            div.addEventListener("mouseover", ()=>{
-               div.setAttribute("class", "gridActive");
+               div.classList.add('gridActive');
            });
            console.log(i);
        };
@@ -52,18 +45,11 @@ function addDiv() {
 
 //This function resets the canvas
  function reset() {
-    container_div = document.querySelector('.container');
-     container_div.parentNode.removeChild('container');  
- };
- 
-
-
- //this function clears the canvas
- function clearCanvas() {
-    let div = document.querySelector('.grid');
-    div.parentNode.removeChild(div);
+     const gridActive_div = document.querySelectorAll('.gridActive');
+     gridActive_div.classList.remove('.gridActive');
+     
  };
 
  //buttons
- resetBtn.addEventListener('click', reset());
- gridBtn.addEventListener('click', addDiv());
+ resetBtn.addEventListener('click', reset);
+ gridBtn.addEventListener('click', addDiv);
